8b8af248aa243bfabcaccfcd709000c711b83dfd
Andrei Neagu <neaguandrei47@gmail.com>
2019-09-15 19:46:50 +0200

    comments and header for task 5 and 6

--- AssemblerApplication Task 5.asm
+++ AssemblerApplication Task 5.asm
@@ -1,10 +1,31 @@
-/*
- * AssemblerApplication5.asm
- *
- *  Created: 2019-09-15 16:24:24
- *   Author: kt222iq
- */ 
-
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;   1DT301, Computer Technology I
+;   Date: 2019-09-09
+;   Author: Andrei Neagu (an223kj)
+;           Konstantinos Tatsis (kt222iq)
+;
+;   Lab number:         1
+;   Title:              How to use the PORTs. Digital input /output.
+;                       Subroutine call.
+;
+;   Hardware:           STK600, CPU ATmega2560
+;
+;   Function:           Ring counter with a delay of 500ms
+;
+;   Input ports:
+;
+;   Output ports:       PORTB
+;
+;   Subroutines:       Delay
+;
+;   Included files:     m2560def.inc
+;
+;   Other information:  Ring counter with a delay of 500ms
+;
+;   Changes in program:
+;						2019-09-09
+;
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  .include "m2560def.inc"
 
  ldi R20, HIGH (RAMEND)
@@ -13,22 +34,22 @@
  ldi R20, low(RAMEND)
  out SPL, R20
 
- ldi r16, 0xFF
+ ldi r16, 0xFF ;PORTB output
  out DDRB, r16
 
- ldi r17, 0b1111_1110
+ ldi r17, 0b1111_1110 ;initial led state
  out PORTB, r17
 
- loop: 
-	 out PORTB, r17
-	 CALL Delay
-	 com r17
-	 LSL r17
-	 com r17
+ loop:
+	 out PORTB, r17 ; initial led state
+	 CALL Delay     ; delay of 0.5sec
+	 com r17        ; this instruction performs a one's complement of register Rd.
+	 LSL r17        ; shifts all bits in Rd one place to the left. Bit 0 is cleared
+	 com r17        ; one's complement again
 rjmp loop
 
 Delay:
-	 ldi r18, 21
+	 ldi r18, 21    ; 500ms delay achieved using calculator
 	 ldi r19, 140
 	 ldi r20, 174
 L1: dec r20
@@ -39,6 +60,3 @@ L1: dec r20
 	brne L1
 	rjmp PC+1
 RET
-
-
-
--- AssemblerApplication Task 6.asm
+++ AssemblerApplication Task 6.asm
@@ -1,49 +1,65 @@
-/*
- * AssemblerApplication6.asm
- *
- *  Created: 2019-09-15 16:40:32
- *   Author: kt222iq
- */ 
-
-
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;   1DT301, Computer Technology I
+;   Date: 2019-09-09
+;   Author: Andrei Neagu (an223kj)
+;           Konstantinos Tatsis (kt222iq)
+;
+;   Lab number:         1
+;   Title:              How to use the PORTs. Digital input /output.
+;                       Subroutine call.
+;
+;   Hardware:           STK600, CPU ATmega2560
+;
+;   Function:           Program with Johnson Counter with an infinite loop
+;
+;   Input ports:
+;
+;   Output ports:       PORTB
+;
+;   Subroutines:        Delay
+;
+;   Included files:     m2560def.inc
+;
+;   Other information:  Program with Johnson Counter with an infinite loop
+;
+;   Changes in program:
+;						2019-09-09
+;
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  .include "m2560def.inc"
 
  ldi r20, HIGH (RAMEND)
- out SPH, R20
+ out SPH, R20          ; SPH high part of the RAMEND
  ldi R20, low (RAMEND)
- out SPL, R20
+ out SPL, R20          ; SPL low pard of RAMEND
 
  ldi r16, 0xFF
- out DDRB, r16
+ out DDRB, r16         ; DDRB is set as an output port
 
- ldi r16, 0xFF
- out PORTB, r16
+ ldi r21, 0b1111_1110  ; initial led state
+ ldi r22, 0xFF         ; leds off
+ ldi r23, 0x00         ; leds on
 
- ldi r21, 0b1111_1110
- ldi r22, 0xFF
- ldi r23, 0x00
+ loop:
+	 out PORTB, r21  ; initial led state
+	 LSL r21         ; shift leds to left
+	 CALL Delay      ; delay of 500ms
 
- loop: 
-	 out PORTB, r21
-	 LSL r21
-	 CALL Delay
-
-	 cp r21, r23
+	 cp r21, r23     ; when r21 equals to r23 go to light
 	 breq light
 rjmp loop
 
 
 light:
-	 out PORTB, r23
-	 CALL Delay
-	 ldi r21, 0b1000_0000
+	 out PORTB, r23        ; leds on
+	 CALL Delay            ; delay of 500ms
+	 ldi r21, 0b1000_0000  ; initialize the leds to go to the right
 	 out PORTB, r21
 	 Sec_loop:
-			out PORTB, r21
-			ASR r21
-			CALL Delay
-			cp r21, r22
-
+			out PORTB, r21     ; initialize the leds to go to the right
+			ASR r21            ; shift the bits to the right
+			CALL Delay         ; delay of 500ms
+			cp r21, r22        ; if r21 equals r22 go back to loop
 			breq loop
 		rjmp Sec_loop
 
@@ -60,7 +76,3 @@ L1:  dec r20
 	 brne L1
 	 rjmp PC+1
 RET
-
-
-
-
